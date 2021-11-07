0c2556b2e443c5c64807586b0438450f49a484d8
ChipmunkForLove <chipmunk4lov@gmail.com>
2021-11-07 12:02:47 +0900

    [Style] global-style 추가, theme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Main from './pages/Main';
 
-function App() {
-  return (
-    <>
-      <div className="App" />
-    </>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <div className="App">
+      <Main />
+    </div>
+  </>
+);
 
 export default App;
---
+++ src/page/Main.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import googleIcon from '../img/googleIcon.svg';
+
+const MainPage = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Brand = styled.div``;
+const LoginBtn = styled.div``;
+
+const MainContent = styled.div``;
+
+const ItemList = styled.div``;
+
+const Main = () => (
+  <MainPage>
+  </MainPage>
+);
+
+export default Main;
