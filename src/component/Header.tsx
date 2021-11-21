ca9653b63ad639c9046a21fc4043152939a222db
ChipmunkForLove <chipmunk4lov@gmail.com>
2021-11-21 19:20:17 +0900

    [Feat] shop menu bar 완료

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -9,22 +9,6 @@ interface Props {
 const Header: React.FC<Props> = ({ user }: Props) => (
   <HeaderDiv>
     <Logo/>
-    <Menu>
-      <MenuText>SHOP ALL</MenuText>
-      <MenuDetail/>
-    </Menu>
-    <Menu>
-      <MenuText>COFFEE</MenuText>
-      <MenuDetail/>
-    </Menu>
-    <Menu>
-      <MenuText>BREWING</MenuText>
-      <MenuDetail/>
-    </Menu>
-    <Menu>
-      <MenuText>SERVEWARE</MenuText>
-      <MenuDetail/>
-    </Menu>
     <Login>{user}</Login>
   </HeaderDiv>
 );
@@ -40,19 +24,6 @@ const Logo = styled(logo)`
   height: 40px;
   margin-left: 60px;
   margin-right: 20px;
-`;
-const Menu = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100px;
-`;
-const MenuText = styled.p`
-  text-align: center;
-`;
-const MenuDetail = styled.div` 
-  width: 70px;
-  border : solid 2px ${(props)=> props.theme.color.PrimaryBG};
 `;
 const Login = styled.div`
 `;
